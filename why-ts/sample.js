6d62098342ede6b9f6d954c4d257f87ddf2e164f
minkyu-dev <park9831.dev@gmail.com>
2023-07-21 16:42:26 +0900

    getting started 추가

---
+++ why-ts/sample.js
@@ -0,0 +1,16 @@
+// function add(a, b) {
+//     return a + b;
+// }
+// let result = add(10, 20);
+// result.toLocaleString();
+
+// @ts-check
+
+/**
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
