59f852941f401b08e8ee41b8492c5f133e24dbb8
minkyu-dev <park9831.dev@gmail.com>
2023-07-24 17:45:57 +0900

    type aliases 추가

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,19 @@
+type Person = {
+    name: string,
+    age: number
+}
+
+interface Person {
+    name: string,
+    age: number
+}
+
+let minkyu: Person = {
+    name: '민규'
+    , age: 33
+}
+
+type Todo = {id: number, title: string, done: boolean};
+function getTodo(todo: Todo): void {
+
+}
